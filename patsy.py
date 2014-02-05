91203c7a7d597aab3c572b60b047d7d65970bf92
Periklis Ntanasis <pntanasis@gmail.com>
2014-02-05 04:15:48 +0200

    some progress.. or not..

--- patsy.py
+++ patsy.py
@@ -1,11 +1,19 @@
 import socket
 import _thread as thread
+import mimetypes
+import os.path
 
 CONFIGURATION = { 
 	'HOST' : '127.0.0.1' , 
 	'PORT' : 31338 , 
 	'MAX_REQUEST' : 1024 * 2,  # 2 MB
-	'DOCUMENT_ROOT' : 'htdocs'
+	'DOCUMENT_ROOT' : 'htdocs', # should use full path
+	'HTTP_VERSION' : 'HTTP/1.0'
+}
+
+STATUS_CODES = {
+	'OK' : '200 OK',
+	'NOT_FOUND' : '404 Not Found'
 }
 
 def handleRequest(clientSocket, address):
@@ -15,27 +23,62 @@ def handleRequest(clientSocket, address):
 	target = ""
 	headers = {}
 	for i in range(0, len(lines)):
-		#print("line: "+lines[i])
 		if method == "" and (lines[i] == b'\r\n'.decode("utf-8") or lines[i] == b'\n'.decode("utf-8")):
 			continue
-		elif method == "":			
-			#print("HERE")
+		elif method == "":
 			args = lines[i].replace(b'\t'.decode("utf-8"), '').split(' ')
 			method = args[0]
 			target = args[1]
-		#print("Method: "+method+" and target "+target)
-		requestHandler[method](clientSocket, address, target, headers)
+		elif not (lines[i] == '' or lines[i] == b'\r\n'.decode("utf-8") or lines[i] == b'\n'.decode("utf-8")):
+			#print("LINE : "+lines[i])
+			header, value = lines[i].split(':',1)
+			headers[header] = value
+			#print("header "+header+" "+headers[header])
+		elif lines[i] == b'\r\n'.decode("utf-8") or lines[i] == b'\n'.decode("utf-8"):
+			break
+	requestHandler[method](clientSocket, address, target, headers)
 		
 def handleGet(clientSocket, address, target, headers):
 	#print("INTO GET!")
+	#print("**START**")
+	uri = getUriName(target)
 	retmsg = """HTTP/1.0 200 OK
 Date: Fri, 31 Dec 1999 23:59:59 GMT
 Content-Type: text/html
-Content-Length: 2
 
-OK"""
-	clientSocket.send(bytes(retmsg,'utf-8'))
-	return False
+THIS IS A FULL MESSAGE
+"""
+	filePath, mime, status = getResource(target)
+	clientSocket.send(bytes(CONFIGURATION['HTTP_VERSION']+" "+status,'utf-8'))
+	clientSocket.send(b'\n')
+	clientSocket.send(bytes("Content-Type: "+mime,'utf-8'))
+	clientSocket.send(b'\r\n\n')
+	if mime[:4] == "text":
+		with open(filePath, 'r') as f:
+			for line in f:
+				clientSocket.send(bytes(line,'utf-8'))
+	else:
+		with open(filePath, 'rb') as f:
+			for line in f:
+				clientSocket.send(line)
+	clientSocket.send(b'\r\n')
+	#print("**END**")
+
+def getUriName(target):
+	# TO IMPLEMENT
+	return target
+	
+def getResource(uri):
+	# return mime type and file descriptor
+	filePath = CONFIGURATION['DOCUMENT_ROOT']+getUriName(uri)
+	status = STATUS_CODES['OK']
+	mime = 'text/html'
+	(a, b) = mimetypes.guess_type(uri)
+	mime = a
+	if not (os.path.isfile(filePath) or os.path.isdir(filePath)):
+		status = STATUS_CODES['NOT_FOUND']
+	#fileD = open(CONFIGURATION['DOCUMENT_ROOT']+uri, 'r')
+	return  filePath, mime, status
 
 requestHandler = {
 	'GET' : handleGet
@@ -48,6 +91,7 @@ if __name__ == '__main__':
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.bind((CONFIGURATION['HOST'], CONFIGURATION['PORT']))
 	server.listen(5)
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	
 	# main loop that accepts http requests
 	while True:
